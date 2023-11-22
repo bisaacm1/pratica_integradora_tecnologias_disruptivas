88ff4eaf1c7aea383c0be6049dbc42580f277775
Isaac Batista <isaacbmaciel@gmail.com>
2023-11-22 12:24:51 -0300

    Consultas adicionais do Spring Data JPA.

--- modulo14/aplicacao/backend/src/main/java/com/projeto/integrado/repository/UserRepository.java
+++ modulo14/aplicacao/backend/src/main/java/com/projeto/integrado/repository/UserRepository.java
@@ -1,11 +1,27 @@
 package com.projeto.integrado.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.projeto.integrado.entity.User;
+import com.projeto.integrado.entity.Projeto;
+import com.projeto.integrado.entity.Recurso;
+import com.projeto.integrado.entity.Tarefa;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
-	Optional<User> findByUserEmail(String email);
+    Optional<User> findByUserEmail(String email);
+}
+
+public interface ProjetoRepository extends JpaRepository<Projeto, Integer> {
+    List<Projeto> findByDescricao(String descricao);
+}
+
+public interface RecursoRepository extends JpaRepository<Recurso, Integer> {
+    Optional<Recurso> findByNomeRecurso(String nomeRecurso);
+}
+
+public interface TarefaRepository extends JpaRepository<Tarefa, Integer> {
+    Optional<Tarefa> findByStatusTarefa(String statusTarefa);
 }
